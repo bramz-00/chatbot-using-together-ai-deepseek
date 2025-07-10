f29e7250b550fd03db271c1d08f11e88ee70a8f8
bramz-00 <zakaria.braham@outlook.com>
2025-07-10 23:43:22 +0100

    update readme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -67,7 +67,7 @@ const App: React.FC = () => {
             {chat.map((msg, index) => (
               <div
                 key={index}
-                className={`flex ${msg.sender === "user" ? "justify-end" : "justify-start"}`}
+                className={` my-2 flex ${msg.sender === "user" ? "justify-end" : "justify-start"}`}
               >
                 <div
                   className={`max-w-[75%] px-4 py-2 rounded-2xl text-sm ${
